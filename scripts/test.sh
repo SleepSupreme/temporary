2d3c550c9d044e448d34effb6840d6f0fc51be64
SleepSupreme <linchenguo0622@gmail.com>
2021-08-09 14:20:16 +0800

    restart the project

--- scripts/test.sh
+++
@@ -1,2 +0,0 @@
-set -ex
-python main.py --exper_name "secure-deep-hiding" --use_key --test --checkpoint "best"
--- scripts/train.sh
+++
@@ -1,2 +0,0 @@
-set -ex
-python main.py --exper_name "secure-deep-hiding" --epochs 80 --use_key --num_secrets 1 --channel_secret 3 --channel_key 3
